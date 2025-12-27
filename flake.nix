aa1b76c24dea2a79ca134e90644adc73836bbb8e
FabianKoehnen <42027473+FabianKoehnen@users.noreply.github.com>
2025-12-27 15:01:14 +0100

    update zephyr to 4.1

--- flake.nix
+++ flake.nix
@@ -8,48 +8,68 @@
     };
   };
 
-  outputs = { self, nixpkgs, zmk-nix }: let
-    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
-  in {
-    packages = forAllSystems (system: rec {
-      default = firmware;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zmk-nix,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    in
+    {
+      packages = forAllSystems (system: rec {
+        default = firmware;
 
-      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-      # firmware = (import ./nix/builders.nix { inherit (nixpkgs.legacyPackages.${system}) callPackage; }).buildSplitKeyboard {
-        name = "firmware";
+        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          # firmware = (import ./nix/builders.nix { inherit (nixpkgs.legacyPackages.${system}) callPackage; }).buildSplitKeyboard {
+          name = "firmware";
 
-        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+          src = nixpkgs.lib.sourceFilesBySuffices self [
+            ".board"
+            ".cmake"
+            ".conf"
+            ".defconfig"
+            ".dts"
+            ".dtsi"
+            ".json"
+            ".keymap"
+            ".overlay"
+            ".shield"
+            ".yml"
+            "_defconfig"
+          ];
 
-        board = "puchi_ble_v1";
-        # shield = "settings_reset";
-        shield = "redsplit_%PART%";
-        parts = [
-          "left nice_view"
-          "right"
-        ];
-        snippets = [
-          "zmk-usb-logging"
-        ];
-        enableZmkStudio = false;
+          board = "puchi_ble";
+          # shield = "settings_reset";
+          shield = "redsplit_%PART%";
+          parts = [
+            "left nice_view"
+            "right"
+          ];
+          snippets = [
+            "zmk-usb-logging"
+          ];
+          enableZmkStudio = false;
 
-        # zephyrDepsHash = "sha256-iQzbYGis8XB9aUYHkVrnnhEc5XoRaHZkWPeSEERSLwI="; #with gestures
-        # zephyrDepsHash = "sha256-rsRz5Cp7l6ULEI0WawnCnNANGIy8UAcjhAQwShxyt6I=";
-        # zephyrDepsHash = "sha256-baYqnMUnZ4CxOj3ThVXwZnIHSIkN2HwLUA0FhMCX3jk=";
-        zephyrDepsHash = "sha256-baYqnMUnZ4CxOj3ThVXwZnIHSIkN2HwLUA0FhMCX3jk=";
+          # zephyrDepsHash = "sha256-iQzbYGis8XB9aUYHkVrnnhEc5XoRaHZkWPeSEERSLwI="; #with gestures
+          # zephyrDepsHash = "sha256-rsRz5Cp7l6ULEI0WawnCnNANGIy8UAcjhAQwShxyt6I=";
+          # zephyrDepsHash = "sha256-baYqnMUnZ4CxOj3ThVXwZnIHSIkN2HwLUA0FhMCX3jk=";
+          zephyrDepsHash = "sha256-zsTDZAZ8dbCvnrpFFx50i6O+is5qA64i66X6Myl1+sA=";
 
-        # meta = {
-        #   description = "ZMK firmware";
-        #   license = nixpkgs.lib.licenses.mit;
-        #   platforms = nixpkgs.lib.platforms.all;
-        # };
-      };
+          # meta = {
+          #   description = "ZMK firmware";
+          #   license = nixpkgs.lib.licenses.mit;
+          #   platforms = nixpkgs.lib.platforms.all;
+          # };
+        };
 
-      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
-      update = zmk-nix.packages.${system}.update;
-    });
+        flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+        update = zmk-nix.packages.${system}.update;
+      });
 
-    devShells = forAllSystems (system: {
-      default = zmk-nix.devShells.${system}.default;
-    });
-  };
+      devShells = forAllSystems (system: {
+        default = zmk-nix.devShells.${system}.default;
+      });
+    };
 }
